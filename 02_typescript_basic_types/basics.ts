f8731665508b09262aaffebcfa659ef339faf9ee
enjay27 <kdkm2002@gmail.com>
2021-05-17 14:21:46 +0900

    04 next generation

--- 02_typescript_basic_types/basics.ts
+++ 02_typescript_basic_types/basics.ts
@@ -1,7 +1,7 @@
 function addNumbersWithPhrase(n1: number, n2: number, showResult: boolean, phrase: string) {
     const result = n1 + n2;
     if (showResult) {
-        console.log(phrase + result);
+        return phrase + result;
     } else {
         return result;
     }
