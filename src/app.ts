a386089e23fcad5be71a0244e6152357512a069d
enjay27 <kdkm2002@gmail.com>
2021-10-27 11:23:48 +0900

    class-transformer

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,32 @@
+import 'reflect-metadata';
+
 import _ from 'lodash';
+// import { plainToClass } from '../node_modules/class-transformer/types/index';
+import { Product } from './product.model';
 
-declare var GLOBAL: string; // it will exist
+import { plainToClass } from 'class-transformer';
 
-console.log(_.shuffle([1, 2, 3]));
+const products = [
+    {
+        title: 'A Carpet',
+        price: 29.99
+    },
+    {
+        title: 'A Book',
+        price: 10.99
+    }
+];
 
-console.log(GLOBAL);
+// const p1 = new Product('A Book', 12.99);
+
+// const loadedProducts = products.map(prod => {
+//     return new Product(prod.title, prod.price);
+// })
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+    console.log(prod.getInformation());
+}
+
+// console.log(p1.getInformation());
